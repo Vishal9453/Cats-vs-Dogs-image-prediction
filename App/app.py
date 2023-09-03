d22a255900d99daa1310bb8cfed151b9ad7c1bab
Vishal Singh <75610231+Vishal9453@users.noreply.github.com>
2023-09-03 22:53:28 +0530

    Update app.py

--- App/app.py
+++ App/app.py
@@ -8,29 +8,39 @@ import io
 app = Flask(__name__)
 CORS(app)
 
+# Load the pre-trained model
 model = keras.models.load_model('Cats_vs_Dogs.model')
 
 @app.route('/')
 def index():
+    # Serve an HTML page
     with open('index.html', 'r') as file:
         html_content = file.read()
     return html_content
 
 @app.route('/predict', methods=['POST'])
 def predict():
+    # Get the image file from the POST request
     imagefile = request.files['imagefile']
 
+    # Read the image file as a binary stream
     img_stream = imagefile.read()
 
+    # Load and preprocess the image
     img = image.load_img(io.BytesIO(img_stream), color_mode='grayscale', target_size=(60, 60))
     img_array = image.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0)
     img_array /= 255.0
 
+    # Make a prediction using the loaded model
     prediction = model.predict(img_array)
+    
+    # Determine the predicted class (Cat or Dog)
     predicted_class = "Dog" if prediction[0][1] > prediction[0][0] else "Cat"
 
+    # Return the prediction as JSON
     return jsonify({'prediction': predicted_class})
 
 if __name__ == '__main__':
+    # Start the Flask app
     app.run()
