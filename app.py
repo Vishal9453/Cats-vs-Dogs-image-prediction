0a5946501d61f9e3e14110ff64bcda2c48d76609
Vishal Singh <75610231+Vishal9453@users.noreply.github.com>
2023-09-03 13:44:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template, jsonify
 from flask_cors import CORS
 import keras
 import numpy as np
-from keras.preprocessing import image
+from PIL import Image
 import io
 
 app = Flask(__name__)
@@ -23,10 +23,14 @@ def predict():
 
     img_stream = imagefile.read()
 
-    img = image.load_img(io.BytesIO(img_stream), color_mode='grayscale', target_size=(60, 60))
-    img_array = image.img_to_array(img)
+    # Load the image using Pillow (PIL)
+    img = Image.open(io.BytesIO(img_stream))
+    img = img.convert('L')  # Convert to grayscale if needed
+    img = img.resize((60, 60))  # Resize to the target size
+    img_array = np.array(img)
+    img_array = img_array / 255.0  # Normalize the image data
+
     img_array = np.expand_dims(img_array, axis=0)
-    img_array /= 255.0
 
     prediction = model.predict(img_array)
     predicted_class = "Dog" if prediction[0][1] > prediction[0][0] else "Cat"
